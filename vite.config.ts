8d07f7e4ac1fb24001c8b9a277e4dddfe0c4dad1
nicatsemedov777 <134422329+nicatsemedov777@users.noreply.github.com>
2025-11-27 13:17:20 +0400

    Set base path for Vite server configuration
    Added base path configuration for the server.

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,13 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true,             // allows external access (important)
-    port: 5173,             // your dev port
+    host: true,
+    port: 5173,
     allowedHosts: [
       'leqalimza.az',
       'www.leqalimza.az',
-      '161.35.208.15',      // your server IP (optional)
-      'vite'                // for docker/nginx internal proxy (optional)
+      '161.35.208.15'
     ]
-  }
+  },
+  base: '/test/', // IMPORTANT!
 })
