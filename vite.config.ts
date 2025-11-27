3c6592bb902bf307ff04ab177b628606d0d3ac3c
nicatsemedov777 <134422329+nicatsemedov777@users.noreply.github.com>
2025-11-27 13:36:12 +0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,13 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/test/',     // IMPORTANT!
   server: {
     allowedHosts: [
       'leqalimza.az',
-      'www.leqalimza.az',
-      '161.35.208.15'
-    ]
+      'www.leqalimza.az'
+    ],
+    host: true,
+    port: 5173
   }
 })
