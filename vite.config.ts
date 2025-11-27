c20ebe404c0058bba7f7aad486243f55438a9fbb
nicatsemedov777 <134422329+nicatsemedov777@users.noreply.github.com>
2025-11-27 13:13:55 +0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,             // allows external access (important)
+    port: 5173,             // your dev port
+    allowedHosts: [
+      'leqalimza.az',
+      'www.leqalimza.az',
+      '161.35.208.15',      // your server IP (optional)
+      'vite'                // for docker/nginx internal proxy (optional)
+    ]
+  }
 })
